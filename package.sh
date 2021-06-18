cb4d9299004d65e0dddbf7fa7709146f5962fef0
Shayekh Mohiuddin Ahmed Navid <shayekhnavid@gmail.com>
2021-06-18 20:07:25 +0600

    first commit

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TARGET_DIR=${PROJECT_ROOT}
+SHIPPER_DIR=${PROJECT_ROOT}/cloudwatch-loki-shipper
+
+deactivate  > /dev/null 2>&1
+
+if [ -d "venv" ]; then
+  rm -rf venv
+fi
+
+python3 -m venv venv
+
+. venv/bin/activate
+pip3 install -r requirements.txt
+
+PACKAGE_DIR=$(find ${PROJECT_ROOT}/venv -name "site-packages")
+
+cd "${PACKAGE_DIR}"
+zip -r9 "${TARGET_DIR}/lambda.zip" .
+
+cd "${SHIPPER_DIR}"
+zip -g "${TARGET_DIR}/lambda.zip" cloudwatch-loki-shipper.py
