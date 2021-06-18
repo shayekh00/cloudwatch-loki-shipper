a32469eef5277b77d8f4b1a3fd5ef8a1f3284634
Shayekh Mohiuddin Ahmed Navid <shayekhnavid@gmail.com>
2021-06-19 00:25:35 +0600

    update

--- publish.sh
+++ publish.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $# != 2 ]]; then
-  echo "usage: $0 <s3_bucket_name> <aws_account_name>"
-  exit 1
-fi
+# if [[ $# != 2 ]]; then
+#   echo "usage: $0 <s3_bucket_name> <aws_account_name>"
+#   exit 1
+# fi
 
-S3_BUCKET="${"loki-shipper-ddl"}"
-ACCOUNT_NAME="${"cloudcreation"}"
+# S3_BUCKET="${"loki-shipper-ddl"}"
+# ACCOUNT_NAME="${"cloudcreation"}"
 
 
-aws s3 cp lambda.zip "s3://${S3_BUCKET}/lambda.zip" --profile "${ACCOUNT_NAME}"
+# aws s3 cp lambda.zip "s3://${S3_BUCKET}/lambda.zip" --profile "${ACCOUNT_NAME}"
+
+
+aws s3 cp lambda.zip "s3://loki-shipper-ddl/lambda.zip" --profile "cloudcreation"
